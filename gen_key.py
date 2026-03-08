9f9cec13ba2bfd664665a9bfd22402d7d5f367ee
Isshaan-Dhar <ishaan.dhar2004@gmail.com>
2026-03-08 15:11:02 +0530

    Update gen_key.py

--- gen_key.py
+++ gen_key.py
@@ -1,7 +1,6 @@
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
 
-# Generate Private Key
 private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
 with open("private.pem", "wb") as f:
     f.write(private_key.private_bytes(
@@ -9,12 +8,12 @@ with open("private.pem", "wb") as f:
         format=serialization.PrivateFormat.PKCS8,
         encryption_algorithm=serialization.NoEncryption()
     ))
-
-# Generate Public Key
+    
 public_key = private_key.public_key()
 with open("public.pem", "wb") as f:
     f.write(public_key.public_bytes(
         encoding=serialization.Encoding.PEM,
         format=serialization.PublicFormat.SubjectPublicKeyInfo
     ))
+
 print("[+] RSA Key pair generated: public.pem and private.pem")
